17475a567b75ba1e8e1461b8cee1f17a0acfaec9
SungMoonPark <taim831@naver.com>
2024-07-29 23:43:22 +0900

    study

--- test_2007.java
+++ test_2007.java
@@ -12,14 +12,23 @@ public class test_2007 {
 			String x = sc.next();
 			int answer = 0;
 			boolean b = false;
-			for(int i = 1; i < 15; i++) {
-				if(x.substring(0, i).equals(x.substring(i+1, i+i+1)) && b==false) {
-					answer = i+1;
-					b=true;
-				}
+            boolean k = true;
+			for(int i = 2; i <= 15; i++) {
+                for(int j = 0; j < 30; j = j+i) { 
+               		if(i+i+j >= 30 || j+i >=30)
+                        break;
+					if(x.substring(j, j+i).equals(x.substring(j+i, (i+i+j) )) == false) {
+                       k = false;       
+                }
+                   if(b== false && k == true){
+                   	b = true; 
+                    answer = i;
 			}
+				}	
+                k = true;
+            }
 			System.out.println("#" + t + " " + answer);
-			
+
 		}
 
 	}
