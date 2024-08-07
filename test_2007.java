77aa0e6bfb0fe16768ea8d1a9889a89e7d9bab8d
SungMoonPark <taim831@naver.com>
2024-08-07 22:19:31 +0900

    Revert "study"
    This reverts commit 623df51a9a4b45ef70b88d64c5905a3f54a5dbff.

--- test_2007.java
+++ test_2007.java
@@ -11,23 +11,24 @@ public class test_2007 {
 		for(int t = 1; t < T+1; t++) {
 			String x = sc.next();
 			int answer = 0;
-			boolean b = false; // 가장 먼저 만족하면 true로
-            boolean k = true; // for문에서 한번이라도 만족하지 않는 경우 false로
-			for(int i = 2; i <= 15; i++) { // 패턴 글자수
-                for(int j = 0; j < 30; j = j+i) { // 글자 인덱스
-               		if(i+i+j >= 30 || j+i >=30) // 글자 인덱스 벗어난 경우
+			boolean b = false;
+            boolean k = true;
+			for(int i = 2; i <= 15; i++) {
+                for(int j = 0; j < 30; j = j+i) { 
+               		if(i+i+j >= 30 || j+i >=30)
                         break;
-					if(x.substring(j, j+i).equals(x.substring(j+i, (i+i+j))) == false) 
+					if(x.substring(j, j+i).equals(x.substring(j+i, (i+i+j) )) == false) {
                        k = false;       
-					
-					if(b== false && k == true){
-                   		b = true; 
-                   		answer = i;
-                   }
-                }	
+                }
+                   if(b== false && k == true){
+                   	b = true; 
+                    answer = i;
+			}
+				}	
                 k = true;
             }
 			System.out.println("#" + t + " " + answer);
+
 		}
 
 	}
